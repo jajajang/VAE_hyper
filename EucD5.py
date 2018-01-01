8cb97c49fc93e581ef52c141b84ddc57724612d6
jajajang <jajajang@kaist.ac.kr>
2018-01-01 15:12:42 +0900

    CUDA doubt

--- EucD5.py
+++ EucD5.py
@@ -21,7 +21,7 @@ parser.add_argument('--batch-size', type=int, default=16, metavar='N',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=Trues,
+parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
